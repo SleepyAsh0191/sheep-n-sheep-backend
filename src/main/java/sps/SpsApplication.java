a963fed072c39b649170182235cd40a4598142e4
WhkSoft <whksoft@gmail.com>
2022-09-15 21:32:06 +0800

    修改自述文件，顺便去除`map_id`校验

--- src/main/java/sps/SpsApplication.java
+++ src/main/java/sps/SpsApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpsApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SpsApplication.class, args);
     }
-
 }
